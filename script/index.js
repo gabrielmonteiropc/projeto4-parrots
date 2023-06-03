9a964370bebcc04a003ae1bf809fc75b4f3ffbd0
Gabriel Dos Santos Monteiro <bhcmonteiro54@gmail.com>
2023-06-03 15:07:28 -0300

    Requisito de perguntas as cartas para o usuário configurada

--- script/index.js
+++ script/index.js
@@ -4,27 +4,21 @@
 
 };*/
 
+// Função que pergunta a quantidade de cartas
 function perguntarCartas() {
 
     // Perguntar com quantas cartas quer jogar
-    const pergunta = Number(prompt('Com quantas cartas você quer jogar?'));
+    let pergunta = Number(prompt('Digite um número de cartas de 4 a 14 e somente números pares'));
 
-    // Só pode inserir números pares de 4 a 14
-    if (pergunta >= 4 && pergunta <= 14) {
-        alert('FUNCIONOU!');
-    } else { // Se for número inválido o prompt tem que ficar se repetindo em Looping até o número ser válido
-        
-        let contadorCartas = 0;
-
-        for (contadorCartas = 0; contadorCartas <= 1; contadorCartas++) {
-            prompt('Número inválido! Digite um número par entre 4 a 14');
-            prompt('Digite um número válido');
-        } 
+    // Usuário só pode inserir números pares de 4 a 14 se não ficar no looping
+    while (pergunta % 2 != 0 || pergunta < 4 || pergunta > 14 && pergunta !== NaN) {
+        pergunta = Number(prompt('Digite um número de cartas de 4 a 14 e somente números pares'));
     }
-}
 
+}
 perguntarCartas()
 
+// Função que faz a virada de cartas 
 function virarCarta() {
     const cartaViradaFront = document.querySelector('.front-face');
     cartaViradaFront.classList.toggle('front');
